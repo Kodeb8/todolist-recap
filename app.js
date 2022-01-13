8149ab785f70804b3fe3f9262a912fd913d566c9
Kodeb8 <kodeb8@tutanota.com>
2022-01-13 13:48:20 -0400

    Add list functionalities

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
+// Express
 const express = require("express");
 const app = express();
+app.use(express.static("public"));
+
+// Bodyparser
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+
+// EJS
 app.set("view engine", "ejs");
 
+// Date
+const date = require(__dirname + "/date.js");
+
+// Global variables
+let items = [];
+let workItems = [];
+
+// List webpage
 app.get("/", function(req, res){
-		res.render("list", {helloWorld: "hello world"});
+	let day = date.getDate();
+	res.render("list", {listHeader: day, listArray: items, listValue: "list"}); 
+});
+
+// Work webpage
+app.get("/work", function(req, res){
+	res.render("list", {listHeader: "Work List", listArray: workItems, listValue: "work"});
+});
+
+// Post requests
+app.post("/", function(req, res){
+	// Todo list
+	const item = req.body.listItem;
+	
+	// Send user to the correct list
+	if(req.body.list === "work"){
+		workItems.push(item);
+		res.redirect("/work");
+	} else {
+		items.push(item);
+		res.redirect("/");
+	};
+	
 });
 
 app.listen(3000, function(){
